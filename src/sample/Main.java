28cfe67914dff40124b51004e82ceb924b689884
Felix Mada <madafelix.m@gmail.com>
2017-11-18 00:18:33 +0200

    serial interface connects and lists sent data

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,20 @@
 package sample;
 
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.nio.charset.StandardCharsets;
+
 
 public class Main extends Application {
 
@@ -18,6 +28,32 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        Gson gson = new GsonBuilder().create();
+        SerialPort serialPort= SerialPort.getCommPorts()[0];
+        serialPort.openPort();
+        System.out.println("opened");
+        serialPort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent serialPortEvent) {
+                if(serialPortEvent.getEventType()!= SerialPort.LISTENING_EVENT_DATA_AVAILABLE) return;
+                if(serialPort.bytesAvailable()==0) return;
+                byte[] newData = new byte[serialPort.bytesAvailable()];
+                int num = serialPort.readBytes(newData, newData.length);
+                String str = new String(newData, StandardCharsets.UTF_8);
+                try {
+                    PatchPacket patchPacket = gson.fromJson(str, PatchPacket.class);
+                    System.out.print(patchPacket.T);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        //launch(args);
     }
 }
---
+++ src/sample/PatchPacket.java
@@ -0,0 +1,10 @@
+package sample;
+
+/**
+ * Created by Felix on 11/17/2017.
+ */
+public class PatchPacket {
+    public int t; // time value
+    public int T; // temperature (in XX.XX format)
+    public int H; // humidity (in XX format
+}
